0bd2a63a6e684227afde352000019b491ff6caaf
silence <imsilence@outlook.com>
2020-02-27 22:05:45 +0800

    [modify]: add restart
    Signed-off-by: silence <imsilence@outlook.com>

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ps -ef | grep testdeploy | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep testdeploy | grep -v grep | grep -v restart | awk '{print $2}' | xargs kill -9
 
 nohup /opt/testdeploy/testdeploy >/dev/null 2>&1 &
